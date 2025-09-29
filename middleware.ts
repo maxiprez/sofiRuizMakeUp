52de2c581499b28f79ba58876add3aa8bd2e12d9
maxiprez <74969313+maxiprez@users.noreply.github.com>
2025-09-29 19:32:35 -0300

    feat: add to middleware not login page if the user is log

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,31 @@
 import { NextRequest } from "next/server";
 import { auth } from "./auth";
 import { NextResponse } from "next/server";
-export const config = { matcher: ['/admin/:path*'] };
+
+export const config = { 
+  matcher: [
+    '/admin/:path*',
+    '/login',
+    '/register',
+    '/resetPassword'
+  ] 
+};
+
+const authRoutes = ['/login', '/register', '/resetPassword'];
 
 export async function middleware(request: NextRequest) {
-    const session = await auth();
-    const { pathname } = request.nextUrl;
-    if (pathname.startsWith('/admin')) {
-      if (!session || session.user.role !== 'admin') {
-        return NextResponse.redirect(new URL('/', request.url));
-      }
+  const session = await auth();
+  const { pathname } = request.nextUrl;
+  
+  if (pathname.startsWith('/admin')) {
+    if (!session || session.user.role !== 'admin') {
+      return NextResponse.redirect(new URL('/', request.url));
     }
+  }
+  if (authRoutes.some(route => pathname.startsWith(route))) {
+    if (session) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  }
   return NextResponse.next();
 }
