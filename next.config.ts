3cc200fa828008f5c5983f4ac54884d9d24c4b02
maxiprez <74969313+maxiprez@users.noreply.github.com>
2025-10-21 14:23:17 -0300

    update serverless brevo

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    transpilePackages: ['@getbrevo/brevo', 'formidable', '@paralleldrive/cuid2'],
 };
 
 export default nextConfig;
